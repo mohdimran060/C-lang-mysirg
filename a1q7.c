63cd86b608c7349dd93476c2252766dbcaf32cff
mohdimran060 <95698385+mohdimran060@users.noreply.github.com>
2022-06-23 22:03:29 +0530

    Add files via upload

---
+++ a1q7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=1;
+    char b='B';
+    float c=2.2;
+
+    printf("%d : It is a int type value\n",a);
+    printf("%c : It is a  char type value\n",b);
+    printf("%f : It is a  float type value\n",c);
+
+}
---
+++ a1q9.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int in;
+    char ch='b';
+    in=ch;
+    printf(" ASCII value of %c is %d",ch,in);
+}
